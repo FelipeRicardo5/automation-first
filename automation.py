304062ea04bf2a4d6c11d896adcff53f67519329
F. Ricardo <112594292+FelipeRicardo5@users.noreply.github.com>
2024-11-24 22:45:33 -0300

    Update automation.py

--- automation.py
+++ automation.py
@@ -7,16 +7,16 @@ from selenium.webdriver.common.action_chains import ActionChains
 import traceback
 import time
 
-# Configurar o driver (exemplo com ChromeDriver)
+# Configurar o driver
 driver = webdriver.Chrome()
 
 try:
-    # Passo 1: Acesse a tela de produtos
+    # Passo 1: Acesso a tela de produtos
     driver.get("https://www.saucedemo.com/")
     driver.maximize_window()
     print("Página carregada com sucesso.")
     
-    # Passo 2: Faça login
+    # Passo 2: Faço login
     driver.find_element(By.XPATH, '//*[@id="user-name"]').send_keys("standard_user")
     time.sleep(1)
     driver.find_element(By.XPATH, '//*[@id="password"]').send_keys("secret_sauce")
@@ -25,27 +25,27 @@ try:
     time.sleep(1)
     print("Login realizado com sucesso.")
 
-    # Esperar a página carregar completamente
+    # Espero a página carregar completamente
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="inventory_container"]')))
     print("Página de produtos carregada.")
     
-    # Passo 3: Adicione um produto ao carrinho
+    # Passo 3: Adiciono um produto ao carrinho
     add_to_cart_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]')))
     add_to_cart_button.click()
     print("Produto adicionado ao carrinho.")
     
-    # Passo 4: Verifique o contador
+    # Passo 4: Verifico o contador
     cart_counter = WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element((By.XPATH, '//*[@id="shopping_cart_container"]/a/span'), "1"))
     print("Contador atualizado para 1.")
     cart_counter = driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a').click()
 
-    # Passo 5: Remover o produto do carrinho
+    # Passo 5: Removo o produto do carrinho
     remove_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="remove-sauce-labs-backpack"]')))
     remove_button.click()
     print("Produto removido do carrinho.")
     time.sleep(1)
 
-    # Verifique se o contador foi atualizado para 0
+    # Verifico se o contador foi atualizado para 0
     WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element((By.XPATH, '//*[@id="shopping_cart_container"]/a'), ""))
     time.sleep(1)
     print("Contador atualizado para 0.")
